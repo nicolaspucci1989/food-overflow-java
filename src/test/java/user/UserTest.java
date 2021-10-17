b17f0ce3b410af3689fb3ca194ea9e604dfa6df4
Nicolas <nicolaspucci1989@gmail.com>
2021-10-17 12:59:52 -0300

    minor changes

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,7 +1,6 @@
 package user;
 
 import enums.Routine;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
