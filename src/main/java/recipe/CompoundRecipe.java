51e8621bdb17b13bb1b24645505106b0909df03d
Nicolas <nicolaspucci1989@gmail.com>
2022-09-15 00:19:06 -0300

    implement methods

--- src/main/java/recipe/CompoundRecipe.java
+++ src/main/java/recipe/CompoundRecipe.java
@@ -7,56 +7,70 @@ import user.User;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class CompoundRecipe extends Recipe{
-    public List<Recipe> subRecipes;
+public class CompoundRecipe extends Recipe {
+  public List<Recipe> subRecipes;
 
-    public CompoundRecipe(User author,
-                          Set<User> collaborators,
-                          List<Recipe> subRecipes) {
-        super(author, collaborators);
-        this.subRecipes = subRecipes;
-    }
+  public CompoundRecipe(User author,
+                        Set<User> collaborators,
+                        List<Recipe> subRecipes) {
+    super(author, collaborators);
+    this.subRecipes = subRecipes;
+  }
 
-    @Override
-    public Set<Ingredient> getIngredients() {
-        return subRecipes
-                .stream()
-                .flatMap(recipe -> recipe.getIngredients().stream())
-                .collect(Collectors.toSet());
-    }
+  @Override
+  public Set<Ingredient> getIngredients() {
+    return subRecipes
+        .stream()
+        .flatMap(recipe -> recipe.getIngredients().stream())
+        .collect(Collectors.toSet());
+  }
 
-    @Override
-    public Float getCalories() {
-        return subRecipes
-                .stream()
-                .map(Recipe::getCalories)
-                .reduce(0f, Float::sum);
-    }
+  @Override
+  public Float getCalories() {
+    return subRecipes
+        .stream()
+        .map(Recipe::getCalories)
+        .reduce(0f, Float::sum);
+  }
 
-    @Override
-    public List<String> getPreparationSteps() {
-        return subRecipes
-                .stream()
-                .map(Recipe::getPreparationSteps)
-                .flatMap(List::stream)
-                .collect(Collectors.toList());
-    }
+  @Override
+  public List<String> getPreparationSteps() {
+    return subRecipes
+        .stream()
+        .map(Recipe::getPreparationSteps)
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+  }
 
-    @Override
-    public Difficulty difficulty() {
-        // TODO: use flatMap
-        return subRecipes
-                .stream()
-                .map(Recipe::difficulty)
-                .collect(Collectors.toSet())
-                .stream()
-                .max(Difficulty::compare)
-                .orElse(null);
-    }
+  @Override
+  public Difficulty difficulty() {
+    // TODO: use flatMap
+    return subRecipes
+        .stream()
+        .map(Recipe::difficulty)
+        .collect(Collectors.toSet())
+        .stream()
+        .max(Difficulty::compare)
+        .orElse(null);
+  }
 
-    public void addSubRecipe(Recipe recipe) {
-        subRecipes.add(recipe);
-    }
+  @Override
+  public void addIngredient(Ingredient ingredient) throws Exception {
+    throw new Exception();
+  }
+
+  @Override
+  public void addPreparationStep(String step_one) throws Exception {
+    throw new Exception();
+  }
+
+  @Override
+  public void setCalories(Float calories) throws Exception {
+    throw new Exception();
+  }
+
+  public void addSubRecipe(Recipe recipe) {
+    subRecipes.add(recipe);
+  }
 }
