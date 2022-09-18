f583dab1657ed31cd6ed8b71187265c04c78ecfa
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-18 19:03:46 -0300

    add todo

--- src/main/java/builders/SimpleRecipeBuilder.java
+++ src/main/java/builders/SimpleRecipeBuilder.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+// TODO: try to use lombok builder
 public class SimpleRecipeBuilder {
   List<String> preparationSteps = new ArrayList<>();
   Set<Ingredient> ingredients = new HashSet<>();
